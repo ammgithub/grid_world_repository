ee36d61c46a9e0f9ff9babd321b5607c28d0fa86
ammgithub <malyscheff@gmail.com>
2017-05-05 16:41:48 -0500

    Working on the initialization.

--- grid_world_project/src/grid_world_module.py
+++ grid_world_project/src/grid_world_module.py
@@ -17,11 +17,51 @@ import pylab as plt
 np.set_printoptions(linewidth = 100, edgeitems = 'all', suppress = True, 
                  precision = 4)
 
-
+class GridWorld(object):
+    """
+    Grid world object
+    """
+    def __init__(self, policy, num_rows, num_cols, current_delta):
+        """
+        Grid world parameters. 
+        
+        Parameters
+        ----------
+        In    : 
+        Out   : 
+        
+        Examples
+        --------
+        grid_world = GridWorld(policy, num_rows, num_cols, current_delta)
+        grid_world = GridWorld(0.25, 4, 4, 1000000)
+        
+        """
+        # User control
+        self.policy = policy
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.current_delta = current_delta
+        
+        # Internal parameters
+        self.MAXITER = 10
+        self.gamma = 1.0
+        self.MAX_DELTA = 0.001
+        
+        
+        
+        
 
     
 if __name__ == '__main__':
     """
     execfile('C:\\Users\\amalysch\\git\\grid_world_repository\\grid_world_project\\src\\grid_world_module.py')
     """
-    pass
+    policy = 0.25
+    num_rows = 4
+    num_cols = 4
+    current_delta = 1000000
+    grid_world = GridWorld(policy, num_rows, num_cols, current_delta)
+    print "grid_world.policy = ", grid_world.policy
+    print "grid_world.num_rows = ", grid_world.num_rows
+    print "grid_world.num_cols = ", grid_world.num_cols
+    print "grid_world.current_delta = ", grid_world.current_delta
