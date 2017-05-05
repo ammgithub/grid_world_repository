65b159054c528c8bef9596ef609961b48b3bfc0e
ammgithub <malyscheff@gmail.com>
2017-05-05 15:56:43 -0500

    Initial commit.

---
+++ grid_world_project/src/grid_world_module.py
@@ -0,0 +1,27 @@
+"""
+Created on May 05, 2017
+
+Grid world example 
+
+Reinforcement Learning, Chapter 4
+(Sutton, Barto, 1998)
+
+"""
+__author__ = 'amm'
+__date__  = "May 05, 2017"
+__version__ = 0.0
+
+import numpy as np
+import pylab as plt
+
+np.set_printoptions(linewidth = 100, edgeitems = 'all', suppress = True, 
+                 precision = 4)
+
+
+
+    
+if __name__ == '__main__':
+    """
+    execfile('C:\\Users\\amalysch\\git\\grid_world_repository\\grid_world_project\\src\\grid_world_module.py')
+    """
+    pass
