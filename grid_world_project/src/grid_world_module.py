8703abff7c12c2350f91e26780b7b409c1238d7f
ammgithub <malyscheff@gmail.com>
2017-07-15 22:50:45 -0500

    Adding another grid world example.

--- grid_world_project/src/grid_world_module.py
+++ grid_world_project/src/grid_world_module.py
@@ -18,53 +18,53 @@ from copy import deepcopy
 np.set_printoptions(linewidth = 100, edgeitems = 'all', suppress = True, 
                  precision = 4)
 
-class GridWorld_TwoArrays(object):
+class GridWorld_Barto(object):
     """
     Grid world using two arrays
-    
+     
     """
     def __init__(self, policy, num_rows, num_cols, reward, verbatim=False):
         """
         Grid world parameters. 
-        
+         
         Parameters
         ----------
         In    : policy (tuple, sum=1) , num_rows, num_cols, reward, verbatim
-        
+         
         Examples
         --------
-        grid_world = GridWorld_TwoArrays(policy, num_rows, num_cols, reward)
-        grid_world = GridWorld_TwoArrays(0.25, 0.25, 0.25, 0.25), 4, 4, -1, True)
+        grid_world = GridWorld_Barto(policy, num_rows, num_cols, reward)
+        grid_world = GridWorld_Barto(0.25, 0.25, 0.25, 0.25), 4, 4, -1, True)
         """
         # User control
-        self.name = "GridWorld_TwoArrays"
+        self.name = "GridWorld_Barto"
         # tuple with policy probabilities (up, down, right, left), sum(policy) = 1
         self.policy = policy
         self.num_rows = num_rows
         self.num_cols = num_cols
         self.reward = reward
         self.verbatim = verbatim
-        
+         
         # Internal parameters
         self.gamma = 1.0
         self.MAXITER = 10
         self.MAXDELTA = 1e-6
         self.counter = 0
         self.current_delta = self.MAXDELTA + 1
-        
+         
     def iterative_policy_evaluation(self):
         """
         This method does most of the work. For all states compute the value 
         function contribution for the possible moves up, down, right, left.  
-        
+         
         There are two possible stopping criteria.  Either number of iterations
         (self.MAXITER) or the maximum change in the value function for a 
         single state (self.MAXDELTA).  
-        
+         
         Parameters
         ----------
         Out   : self.value, self.counter, self.current_delta
-        
+         
         Examples
         --------
         grid_world.iterative_policy_evaluation()
@@ -73,12 +73,12 @@ class GridWorld_TwoArrays(object):
         self.value_new = np.zeros((self.num_rows, self.num_cols))
         counter = self.counter
         current_delta = self.current_delta
-        
+         
         if self.verbatim: 
             print "self.value_new = \n", self.value_new
             print "counter = ", counter
             print "current_delta = ", current_delta, "\n"
-        
+         
         while (current_delta > self.MAXDELTA and 
                counter < grid_world.MAXITER):
             current_delta = 0.0
@@ -88,7 +88,7 @@ class GridWorld_TwoArrays(object):
                     if (r==0 and c==0) or (r==num_rows-1 and c==num_cols-1):
                         # disregard the two terminal states
                         continue
-                    
+                     
                     # v to keep track of progress
                     v = self.value_old[r, c]
                     # Compute value contribution for each action
@@ -100,18 +100,18 @@ class GridWorld_TwoArrays(object):
                                         self.policy[2], reward, self.gamma, self.value_old)
                     mleft = move_left(r, c, self.num_rows, self.num_cols, 
                                       self.policy[3], reward, self.gamma, self.value_old)
-                    
+                     
                     self.value_new[r, c] = mup + mdown + mright + mleft
                     current_delta = max(current_delta, abs(v - self.value_new[r, c]))
-            
+             
             self.value_old = deepcopy(self.value_new)
             counter += 1
-            
+             
             if self.verbatim: 
                 print "self.value_new = \n", self.value_new
                 print "counter = ", counter
                 print "current_delta = ", current_delta, "\n"
-        
+         
         self.value = self.value_new
         self.counter = counter
         self.current_delta = current_delta
@@ -212,28 +212,142 @@ def move_left(r, c, num_rows, num_cols, policy, reward, gamma, value_old):
         mleft = policy * (reward + gamma*value_old[r,c-1])
     return mleft
     
+class GridWorld(object):
+    """
+    4 x 3 grid world from UC CS 188. 
+    
+    Parameters
+    ----------
+    In    : policy (tuple, sum=1) , num_rows, num_cols, reward, verbatim
+    
+    Examples
+    --------
+    gw = GridWorld(policy, num_rows, num_cols, reward)
+    gw = GridWorld(0.25, 0.25, 0.25, 0.25), 4, 4, -1, True)
+    """
+    def __init__(self, num_rows, num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        
+
+    
+
 if __name__ == '__main__':
     """
     execfile('C:\\Users\\amalysch\\git\\grid_world_repository\\grid_world_project\\src\\grid_world_module.py')
     """
-    policy = (0.25, 0.25, 0.25, 0.25)
-#     policy = (0.7, 0.1, 0.1, 0.1)
-#     policy = (1.0, 0.0, 0.0, 0.0)
-    num_rows = 4
-    num_cols = 4
-    reward = -1
-    grid_world = GridWorld_TwoArrays(policy, num_rows, num_cols, reward)
-    print "\nRunning Grid World with two arrays..."
-    print "grid_world.num_rows = ", grid_world.num_rows
-    print "grid_world.num_cols = ", grid_world.num_cols
-    print "grid_world.reward = ", grid_world.reward
-    print "grid_world.gamma = ", grid_world.gamma
-    print "grid_world.MAXDELTA = ", grid_world.MAXDELTA
-    print "grid_world.MAXITER = ", grid_world.MAXITER
-    grid_world.iterative_policy_evaluation()
-    print "\ngrid_world.value = \n", grid_world.value
-    print "grid_world.counter = ", grid_world.counter
-    print "grid_world.current_delta = ", grid_world.current_delta
+
+    print "\n"
+    print 60 * '-'
+    print 18 * ' ' + " Grid World Exercises "
+    print 60 * '-'
+    print "(1) Grid World by Sutton/ Barto."
+    print "(2) Grid World 4 x 3 from UC CS 188."
+    print 60 * '-'
+
+#     invalid_input = True
+#     while invalid_input:
+#         try:
+#             user_in = int(raw_input("Make selection (1)-(2): "))
+#             invalid_input = False
+#         except ValueError as e:
+#             print "%s is not a valid selection. Please try again. "\
+#             %e.args[0].split(':')[1]
+    user_in = 2
+
+    if user_in == 1:
+        print "(1) Grid World by Sutton/ Barto..."
+        policy = (0.25, 0.25, 0.25, 0.25)
+    #     policy = (0.7, 0.1, 0.1, 0.1)
+    #     policy = (1.0, 0.0, 0.0, 0.0)
+        num_rows = 4
+        num_cols = 4
+        reward = -1
+        grid_world = GridWorld_Barto(policy, num_rows, num_cols, reward)
+        print "grid_world.num_rows = ", grid_world.num_rows
+        print "grid_world.num_cols = ", grid_world.num_cols
+        print "grid_world.reward = ", grid_world.reward
+        print "grid_world.gamma = ", grid_world.gamma
+        print "grid_world.MAXDELTA = ", grid_world.MAXDELTA
+        print "grid_world.MAXITER = ", grid_world.MAXITER
+        grid_world.iterative_policy_evaluation()
+        print "\ngrid_world.value = \n", grid_world.value
+        print "grid_world.counter = ", grid_world.counter
+        print "grid_world.current_delta = ", grid_world.current_delta
+    elif user_in == 2:
+        # UC CS 188 Experiment
+        print "(2) Grid World 4 x 3 from UC CS 188...\n"
+        num_rows = 3
+        num_cols = 4
+        gw = GridWorld(num_rows, num_cols)
+        
+        # Transition probability UP
+        pup = np.array([[ 0.9,  0.1,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0.1,  0.8,  0.1,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0.1,  0.8,  0.1,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  1. ,  0. ],
+       [ 0.8,  0. ,  0. ,  0. ,  0.2,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0.8,  0. ,  0. ,  0.1,  0.1,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  1. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0.8,  0. ,  0. ,  0.1,  0.1,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0.1,  0.8,  0.1,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0.8,  0. ,  0. ,  0.1,  0. ,  0.1,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0.8,  0. ,  0. ,  0.1,  0.1,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ]])
+        
+        # Transition probability DOWN
+        pdown = np.array([[ 0.1,  0.1,  0. ,  0. ,  0.8,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0.1,  0.8,  0.1,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0.1,  0. ,  0.1,  0. ,  0.8,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  1. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0.2,  0. ,  0. ,  0.8,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0.1,  0.1,  0. ,  0. ,  0.8,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  1. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0.9,  0.1,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0.1,  0.8,  0.1,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0.1,  0.8,  0.1,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0.1,  0.9,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ]])
+
+        # Transition probability LEFT
+        pleft = np.array([[ 0.9,  0. ,  0. ,  0. ,  0.1,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0.8,  0.2,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0.8,  0.1,  0. ,  0. ,  0.1,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  1. ,  0. ],
+       [ 0.1,  0. ,  0. ,  0. ,  0.8,  0. ,  0. ,  0.1,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0.1,  0. ,  0. ,  0.8,  0. ,  0. ,  0. ,  0.1,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  1. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0.1,  0. ,  0. ,  0.9,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0.8,  0.2,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0.1,  0. ,  0. ,  0.8,  0.1,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0.1,  0. ,  0. ,  0.8,  0.1,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ]])
+
+        # Transition probability RIGHT
+        pright = np.array([[ 0.1,  0.8,  0. ,  0. ,  0.1,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0.2,  0.8,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0.1,  0.8,  0. ,  0.1,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  1. ,  0. ],
+       [ 0.1,  0. ,  0. ,  0. ,  0.8,  0. ,  0. ,  0.1,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0.1,  0. ,  0. ,  0. ,  0.8,  0. ,  0. ,  0.1,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  1. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0.1,  0. ,  0. ,  0.1,  0.8,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0.2,  0.8,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0.1,  0. ,  0. ,  0. ,  0.1,  0.8,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0.1,  0. ,  0. ,  0. ,  0.9,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ],
+       [ 0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ,  0. ]])
+        P = np.stack([pup, pdown, pleft, pright], axis=2)
+        
+        print gw.num_rows
+        print gw.num_cols
+    else:
+        print "Invalid selection. Program terminating. "
+    print "Finished."
+
 
     
     
